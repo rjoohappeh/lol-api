a9eeba84ba1899b2dbc252c1f0e772adbd8db9b2
rjoohappeh <tyler153@gmail.com>
2023-09-16 18:54:56 -0600

    added return type to the getAccountByName controller function

--- src/user-account/user-account.controller.ts
+++ src/user-account/user-account.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { UserAccountService } from './user-account.service';
+import { LoLAccountDto } from './user-account-types';
+import { Observable } from 'rxjs';
 
 @Controller('user-account')
 export class UserAccountController {
@@ -7,7 +9,7 @@ export class UserAccountController {
     constructor(private readonly userAccountService: UserAccountService) {}
 
     @Get('/:name')
-    getAccountByName(@Param('name') name: string) {
+    getAccountByName(@Param('name') name: string): Observable<LoLAccountDto> {
         return this.userAccountService.getAccountByName(name);
     }
 }
